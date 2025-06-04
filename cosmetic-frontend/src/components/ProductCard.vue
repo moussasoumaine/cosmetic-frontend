8b3f8b456a239b0a9b3d9d3125519e2084b01133
moussasoumaine <msoumainebachar@gmail.com>
2025-06-04 03:02:10 +0000

    nouveau fichier .vue pour le porps

---
+++ cosmetic-frontend/src/components/ProductCard.vue
@@ -0,0 +1,74 @@
+
+<template>
+    <div class="card">
+        <img :src="product.image" alt="Image du produit" />
+        <h2>{{ product.name }}</h2>
+        <p>{{ product.description }}</p>
+        <p><strong>{{ product.price }} Fçfa</strong></p>
+        <p>Catégorie : {{ product.category }}</p>
+        <p>Marque : {{ product.brand }}</p>
+
+        <button class="buy-btn" @click="$emit('voir-fiche', product)">
+        🛒 Acheter
+        </button> <br>
+        <button class="buy-btn" @click="$emit('ajouter-au-panier', product)">
+        🛍️ Ajouter au panier
+        </button>
+
+
+   </div>
+</template>
+
+<script>
+        export default {
+        name: 'ProductCard',
+        props: {
+            product: Object
+        },
+        methods: {
+            acheterProduit(nom) {
+                alert(`Vous avez choisi d'acheter : ${nom}`);
+            }
+        }
+
+        }
+</script>
+
+
+<style scoped>
+        .card {
+        width: 250px;
+        height: auto;
+        border: 1px solid #6f0e0e;
+        border-radius: 8px;
+        padding: 30px;
+        box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
+        flex-shrink: 0;
+        box-sizing: border-box;
+       
+      }
+
+        .card img {
+        width: 100%;
+        height: auto;
+        border-radius: 4px;
+        
+        }
+        /* Boutton d'achat */
+        .buy-btn {
+            margin-top: 10px;
+            background-color: #28a745;
+            color: white;
+            border: none;
+            padding: 8px 12px;
+            border-radius: 5px;
+            font-size: 14px;
+            cursor: pointer;
+            width: 100%;
+        }
+
+         .buy-btn:hover {
+            background-color: #218838;
+         }
+
+</style>
